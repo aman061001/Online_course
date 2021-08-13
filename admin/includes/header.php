d06df8a29cd63a74db62c1b2a70ccf48693e929f
Aman Singh <amansingh061001@gmail.com>
2021-08-13 15:41:25 +0530

    First commit

---
+++ admin/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header ">
+                <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button> -->
+                 <a class="navbar-brand" href="index.php" target="_blank" style="color:#fff; margin-left:250px;font-size:45px;4px;margin-bottom:30px;width:1000px;line-height:24px; ">
+                   Online Course Regestration
+                </a>
+            </div>
+            <!-- <div class="left-div">
+            <i class="fa fa-user-plus login-icon" style="align" ></i>  
+            </div> -->
+            
+        </div>
+    </div>
